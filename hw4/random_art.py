4e9896a2ecf15155dfb33391c5d06d5819c2e403
Julian <julian@julian-Latitude-E6420.(none)>
2014-02-19 10:05:19 -0500

    random_art.py

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -10,6 +10,9 @@ from random import randint
 import Image
 
 def build_random_function(min_depth, max_depth):
+    """ builds the random function using recursion. The if and else statements are
+    for making sure that the depth is greater than min_depth and less than max_depth)
+    """
     if max_depth<=0:
         xylist=[["x"],["y"]]
         return xylist[randint(0,1)]
@@ -30,7 +33,9 @@ def build_random_function(min_depth, max_depth):
 
 from math import *
 def evaluate_random_function(f, x, y):
-    
+    """evaluates the random function created above, using recursion. The if/elif statments 
+    are for evaluating different operations.
+    """
     
     if f[0]=='x':
             return x
@@ -59,7 +64,9 @@ def remap_interval(val, input_interval_start, input_interval_end, output_interva
         to the output interval [output_interval_start, output_interval_end].  The mapping
         is an affine one (i.e. output = input*c + b).
     
-        TODO: please fill out the rest of this docstring
+        The mapping includes multiplying the interval by a scaler, and then shifting the interval.
+        The scaling variable was calculated by taking the ratio of the magnitude of the two intervals.
+        The final value is calculated using the scaler and then shifting it by the value of the output_interval_start.
     """
     
     scaling=(output_interval_end-output_interval_start)/float(input_interval_end-input_interval_start)
@@ -71,15 +78,20 @@ def remap_interval(val, input_interval_start, input_interval_end, output_interva
 
 import time
     
-def finalimage(IMAGESIZE,mindepth,maxdepth):   
+def finalimage(IMAGESIZE,mindepth,maxdepth): 
+    """
+    This function takes in the image size, and the min and max depth of the function you want to generate. 
+    The 4 variables at the top builds the 3 functions for each color, and the blank image.
+    Then, the for loop is used for x,y coordinates of each pixel. a and b remaps the interval of 0 to 349 
+    to -1 to 1. Then those values are used to evaluate the random functions generated at the top for each color.
+    And finally, the generated values for the r,g,b are mapped to fit between 0 and 255.
+    These red,green and blue values are put onto the pixel, and this will repeat for all pixels.
+    Then, the image is saved in the current folder for us to enjoy!
+    """
     redfunc= build_random_function(mindepth,maxdepth)
     bluefunc= build_random_function(mindepth,maxdepth)
     greenfunc= build_random_function(mindepth,maxdepth)
     im = Image.new("RGB",(IMAGESIZE,IMAGESIZE))
- 
-    g=evaluate_random_function(greenfunc,200,108)
-    green=remap_interval(g,-1,1,0,255)
-    
     
     for x in range(0,IMAGESIZE):
         for y in range(0,IMAGESIZE):
